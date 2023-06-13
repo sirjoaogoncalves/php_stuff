c97a7b2820894998b18b4c5c4ae23ccac7451ad1
sirjoaogoncalves <joaogon85@gmail.com>
2023-06-13 15:47:06 +0100

    update

---
+++ exercices/ROLLDICE.go
@@ -0,0 +1,53 @@
+//--Summary:
+//  Create a program that can perform dice rolls. The program should
+//  report the results as detailed in the requirements.
+//
+//--Requirements:
+//* Print the sum of the dice roll
+//* Print additional information in these cirsumstances:
+//  - "Snake eyes": when the total roll is 2, and total dice is 2
+//  - "Lucky 7": when the total roll is 7
+//  - "Even": when the total roll is even
+//  - "Odd": when the total roll is odd
+//* The program must handle any number of dice, rolls, and sides
+//
+//--Notes:
+//* Use packages from the standard library to complete the project
+
+package main
+
+import "fmt"
+import "math/rand"
+import "time"
+
+func roll(sides int) int {
+	return rand.Intn(sides) + 1
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	dice, sides := 2, 6
+	rolls := 2
+
+	for r := 1; r <= rolls; r++{
+		sum := 0
+
+		for d := 1; d <= dice; d++ {
+			rolled := roll(sides)
+			sum += rolled
+		 			fmt.Printf("Roll #%d: %d\n", d, rolled)	
+		}
+		fmt.Println("Sum of roll: ", sum)
+		switch sum := sum; {
+			case sum == 2 && dice == 2:
+				fmt.Println("Snake eyes!")
+			case sum == 7:
+				fmt.Println("Lucky 7!")
+			case sum % 2 == 0:
+				fmt.Println("Even!")
+			case sum % 2 == 1:
+				fmt.Println("Odd!")
+		}
+	}
+}
+
