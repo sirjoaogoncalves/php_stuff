ca1491fa3a4383fb852514f6260c72359f5ed8c6
sirjoaogoncalves <joaogon85@gmail.com>
2023-06-15 15:55:56 +0100

    update

---
+++ arrays.go
@@ -0,0 +1,54 @@
+//--Summary:
+//  Create a program that can store a shopping list and print
+//  out information about the list.
+//
+//--Requirements:
+//* Using an array, create a shopping list with enough room
+//  for 4 products
+//  - Products must include the price and the name
+//* Insert 3 products into the array
+//* Print to the terminal:
+//  - The last item on the list
+//  - The total number of items
+//  - The total cost of the items
+//* Add a fourth product to the list and print out the
+//  information again
+
+package main
+
+import "fmt"
+
+type Product struct {
+	price int
+	name  string
+}
+
+func  printStats(list [4]Product){
+	var cost, totalItems int
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		cost += item.price
+
+		if item.name != "" {
+			totalItems += 1
+		}
+	}
+	fmt.Println("Last item on the list:", list[totalItems-1])
+	fmt.Println("Total number of items:", totalItems)
+	fmt.Println("Total cost of items:", cost)
+}	
+
+func main() {
+shoppingList := [4]Product{
+		{1, "Banana"},
+		{6, "Apple"},
+		{3, "Orange"},
+	}
+	printStats(shoppingList)
+
+	shoppingList[3] = Product{4, "Pear"}
+
+	printStats(shoppingList)
+
+
+}
